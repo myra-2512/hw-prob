3a7d08c747e5997b685d909dc88c9cad491a7a3b
myra-2512 <abhi.back2back@gmail.com>
2026-05-31 09:49:19 +0530

    Add files via upload

---
+++ a2.py
@@ -0,0 +1,10 @@
+
+red_balls = 1
+blue_balls = 6
+white_balls = 3
+
+total_balls = red_balls + blue_balls + white_balls
+
+probability_second_white_given_first_white = white_balls / total_balls
+
+print(f"The probability that the second ball is white given the first is white is: {probability_second_white_given_first_white}")
